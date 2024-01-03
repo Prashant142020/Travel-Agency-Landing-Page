9e5bf5bc7cc5ef2d034b2321e22065435aff5586
Prashant142020 <prashant142020@gmail.com>
2024-01-04 01:01:01 +0530

    NabBar Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Test from '@/components/Test'
+import NavBar from '@/components/common/NavBar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
 
-      <Test />
+      <NavBar />
     </main>
   )
 }
