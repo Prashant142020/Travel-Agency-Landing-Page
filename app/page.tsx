8f3e15d54f2e9bcb2b739052ae6a8b622ab5e915
Prashant142020 <prashant142020@gmail.com>
2024-01-04 01:10:03 +0530

    Hero Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Test from '@/components/Test'
 import NavBar from '@/components/common/NavBar'
+import HeroSection from '@/components/sections/HeroSection'
 import Image from 'next/image'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
 
       <NavBar />
+      <HeroSection />
     </main>
   )
 }
